e7d9207ced51137a616b669fa635b8ca60b20e23
Hexhare <66854340+Hexhare@users.noreply.github.com>
2020-09-11 18:21:14 -0700

    Added teleport after fall

---
+++ platformer3d/Assets/_ryan/Scripts/TeleportAfterFall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportAfterFall : MonoBehaviour
+{
+    Vector3 startposition;
+    public float minimumY = -20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startposition = this.transform.position;
+        Debug.Log("Hello World" + transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < minimumY)
+        {
+            transform.position = startposition;
+        }
+        
+    }
+}
